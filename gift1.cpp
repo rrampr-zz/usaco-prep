70338ac0e3093d2ef7bac11355186c4d5be9070c
rrampr <rrampr@umich.edu>
2015-08-24 06:56:38 +0530

    Greedy Gift Givers problem from section 1.1

--- gift1.cpp
+++ gift1.cpp
@@ -13,11 +13,11 @@ TASK: gift1
 using namespace std;
 
 struct Acct {
-	int deposit;
+    int deposit;
 
-	Acct(int cur) : deposit(cur) {}
+    Acct(int cur) : deposit(cur) {}
 
-	Acct() : deposit(0) {}
+    Acct() : deposit(0) {}
 };
 
 vector<string> names;
@@ -25,36 +25,36 @@ unordered_map<string, Acct> accounts;
 
 
 int main() {
-	ifstream fin("gift1.in");
-	ofstream fout("gift1.out");
+    ifstream fin("gift1.in");
+    ofstream fout("gift1.out");
 
-	int np;
-	fin >> np;
+    int np;
+    fin >> np;
 
-	for (int i = 0; i < np; ++i) {
-		string name;
-		fin >> name;
-		names.push_back(name);
-	}
+    for (int i = 0; i < np; ++i) {
+        string name;
+        fin >> name;
+        names.push_back(name);
+    }
 
-	for (int i = 0; i < np; ++i) {
-		string name;
-		fin >> name;
-		int initial, num_people;
-		fin >> initial >> num_people;
-		if (num_people == 0) {
-			continue;
-		}
-		accounts[name].deposit += (-1 * (initial/num_people) * num_people);
-		for (int j = 0; j < num_people; ++j) {
-			string receiver;
-			fin >> receiver;
-			accounts[receiver].deposit += initial/num_people;
-		}
-	}
+    for (int i = 0; i < np; ++i) {
+        string name;
+        fin >> name;
+        int initial, num_people;
+        fin >> initial >> num_people;
+        if (num_people == 0) {
+            continue;
+        }
+        accounts[name].deposit += (-1 * (initial/num_people) * num_people);
+        for (int j = 0; j < num_people; ++j) {
+            string receiver;
+            fin >> receiver;
+            accounts[receiver].deposit += initial/num_people;
+        }
+    }
 
-	for (auto name : names) {
-		fout << name << " " << accounts[name].deposit << "\n";
-	}
-	return 0;
+    for (auto name : names) {
+        fout << name << " " << accounts[name].deposit << "\n";
+    }
+    return 0;
 }
