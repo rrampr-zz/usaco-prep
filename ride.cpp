c729ba777c8892a9ba7ed1fbd71a4b234f69abc7
rrampr <rrampr@umich.edu>
2015-08-24 06:55:20 +0530

    Your ride is here problem from section 1.1

--- ride.cpp
+++ ride.cpp
@@ -13,24 +13,24 @@ using namespace std;
 int encrypt(std::string &, int mod = 47);
 
 int main() {
-	string name,name1;
-	fstream input("ride.in", std::fstream::in), output("ride.out", std::fstream::out);
+    string name,name1;
+    fstream input("ride.in", std::fstream::in), output("ride.out", std::fstream::out);
 
-	input >> name >> name1;
+    input >> name >> name1;
 
-	if(encrypt(name) == encrypt(name1))
-		output << "GO\n";
-	else
-		output << "STAY\n";
+    if(encrypt(name) == encrypt(name1))
+        output << "GO\n";
+    else
+        output << "STAY\n";
 
-	return 0;
+    return 0;
 }
 
 int encrypt(std::string &name, int mod) {
-	int a = 1;
-	for(auto it:name) {
-		a *= (it - 'A' + 1);
-		a %= mod;
-	}
-	return a;
+    int a = 1;
+    for(auto it:name) {
+        a *= (it - 'A' + 1);
+        a %= mod;
+    }
+    return a;
 }
