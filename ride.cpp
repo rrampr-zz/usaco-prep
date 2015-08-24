9fae3373fa7c5710d684c567f9377bed00e0522e
rrampr <rrampr@umich.edu>
2015-08-24 06:50:07 +0530

    Your Ride is here problem from section 1.1

---
+++ ride.cpp
@@ -0,0 +1,36 @@
+/*
+ID: rohitbo1
+LANG: C++11
+PROG: ride
+*/
+
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int encrypt(std::string &, int mod = 47);
+
+int main() {
+	string name,name1;
+	fstream input("ride.in", std::fstream::in), output("ride.out", std::fstream::out);
+
+	input >> name >> name1;
+
+	if(encrypt(name) == encrypt(name1))
+		output << "GO\n";
+	else
+		output << "STAY\n";
+
+	return 0;
+}
+
+int encrypt(std::string &name, int mod) {
+	int a = 1;
+	for(auto it:name) {
+		a *= (it - 'A' + 1);
+		a %= mod;
+	}
+	return a;
+}
